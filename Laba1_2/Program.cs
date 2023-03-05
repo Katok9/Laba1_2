7bace6fc6b4b3a3828968729e5a81c13a6963969
Катерина Шевченко <katya14shevchenko@gmail.com>
2023-03-05 19:37:20 +0200

    Добавьте файлы проекта.

---
+++ Laba1_2/Program.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+
+namespace Laba1_2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Dictionary<string, string> dictionary = new Dictionary<string, string>();
+
+            dictionary.Add("I", "S001");
+            dictionary.Add("II", "S002");
+            dictionary.Add("III", "S001");
+            dictionary.Add("IV", "S005");
+            dictionary.Add("V", "S005");
+            dictionary.Add("VI", "S009");
+            dictionary.Add("VII", "S007");
+
+            Dictionary<string, string> uniqueDictionary = new Dictionary<string, string>();
+
+            foreach (KeyValuePair<string, string> pair in dictionary)
+            {
+                if (uniqueDictionary.ContainsValue(pair.Value) == false)
+                {
+                    uniqueDictionary.Add(pair.Key, pair.Value);
+                }
+            }
+
+            string json = JsonConvert.SerializeObject(uniqueDictionary);
+
+            string path = "json.txt";
+
+            using (StreamWriter writer = new StreamWriter(path))
+            {
+                writer.WriteLine(json);
+            }
+
+        }
+    }
+}
